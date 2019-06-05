45d1159b332c239849a0d5e5cf3764bbaf964006
Mathot1 <47067403+Mathot1@users.noreply.github.com>
2019-06-05 21:55:43 +0200

    Add files via upload

---
+++ Abstrakt_2/Abstrakt_2/Rectangle.cs
@@ -0,0 +1,10 @@
+﻿namespace Abstrakt_2
+{
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            System.Console.WriteLine("Draw a rectangle");
+        }
+    }
+}
---
+++ Abstrakt_2/Abstrakt_2/Shape.cs
@@ -0,0 +1,19 @@
+﻿namespace Abstrakt_2
+{
+    public abstract class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public abstract void Draw();
+        
+        public void Copy()
+        {
+            System.Console.WriteLine("Copy shape into clipboard");
+        }
+        public void Select()
+        {
+            System.Console.WriteLine("Select the shape");
+        }
+
+    }
+}
