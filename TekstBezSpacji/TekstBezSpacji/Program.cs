27a13b8a6dc6b3fb20e812c8e8699861ef96db97
Mathot1 <47067403+Mathot1@users.noreply.github.com>
2019-06-05 22:18:23 +0200

    Add files via upload

---
+++ TekstBezSpacji/TekstBezSpacji/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TekstBezSpacji
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var x = input.Split(' ');
+            var y = new List<string>();
+            int a = x.Length;
+            foreach (var item in x)
+            {
+                y.Add(item.ToLower());
+            }
+            Console.WriteLine(Text(y, a));
+        }
+        public static string Text(List <string> y, int f)
+        {
+            var z = new StringBuilder();
+            for (int i = 0; i < f; i++)
+            {
+                z.Append(char.ToUpper(y[i][0]) + y[i].Substring(1));
+            }
+            return Convert.ToString(z);
+        }
+    }
+}
